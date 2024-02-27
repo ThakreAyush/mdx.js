a39293f04f3b58dba41ebb36079739076e3fc87b
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2024-02-28 00:07:29 +0530

    install mdx pakages for react and mdx loader

---
+++ esm.sh
@@ -0,0 +1,26 @@
+import {MDXProvider} from 'https://esm.sh/@mdx-js/react@3'
+
+
+<script type="module">
+  import {MDXProvider} from 'https://esm.sh/@mdx-js/react@3?bundle'
+</script>
+
+import {MDXProvider} from '@mdx-js/react'
+import Post from './post.mdx'
+// ^-- Assumes an integration is used to compile MDX to JS, such as
+// `@mdx-js/esbuild`, `@mdx-js/loader`, `@mdx-js/node-loader`, or
+// `@mdx-js/rollup`, and that it is configured with
+// `options.providerImportSource: '@mdx-js/react'`.
+
+/** @type {import('mdx/types.js').MDXComponents} */
+const components = {
+  em(properties) {
+    return <i {...properties} />
+  }
+}
+
+console.log(
+  <MDXProvider components={components}>
+    <Post />
+  </MDXProvider>
+)
